7863515509adc1461339b4d3587ff3d0de2f9bb0
Jeff Caddel <jcaddel@yahoo.com>
2015-01-15 17:41:55 -0700

    touchup unit test

--- src/test/java/org/kuali/common/jute/base/TimedIntervalTest.java
+++ src/test/java/org/kuali/common/jute/base/TimedIntervalTest.java
@@ -18,43 +18,25 @@ package org.kuali.common.jute.base;
 import static com.google.common.base.Stopwatch.createStarted;
 import static com.google.inject.Guice.createInjector;
 
-import java.util.List;
-
 import org.junit.Test;
-import org.kuali.common.jute.enc.openssl.OpenSSLModule;
 import org.kuali.common.jute.env.EnvModule;
 import org.kuali.common.jute.json.JsonService;
 import org.kuali.common.jute.json.jackson.JacksonModule;
 import org.kuali.common.jute.system.SystemModule;
 
 import com.google.common.base.Stopwatch;
-import com.google.common.collect.Lists;
-import com.google.inject.AbstractModule;
-import com.google.inject.Guice;
 import com.google.inject.Injector;
 
 public class TimedIntervalTest extends BaseUnitTest {
 
     @Test
     public void test() {
-        try {
-
-            List<AbstractModule> modules = Lists.newArrayList();
-            modules.add(new SystemModule());
-            modules.add(new EnvModule());
-            modules.add(new OpenSSLModule());
-            modules.add(new JacksonModule());
-            Injector injectorr = Guice.createInjector(modules);
-
-            Stopwatch sw = createStarted();
-            Injector injector = createInjector(new SystemModule(), new EnvModule(), new JacksonModule());
-            JsonService json = injector.getInstance(JsonService.class);
-            TimedInterval timing = TimedInterval.build(sw);
-            show(json, timing);
-            checkPerfectReadWrite(json, timing, TimedInterval.class);
-        } catch (Throwable e) {
-            e.printStackTrace();
-        }
+        Stopwatch sw = createStarted();
+        Injector injector = createInjector(new SystemModule(), new EnvModule(), new JacksonModule());
+        JsonService json = injector.getInstance(JsonService.class);
+        TimedInterval timing = TimedInterval.build(sw);
+        show(json, timing);
+        checkPerfectReadWrite(json, timing, TimedInterval.class);
     }
 
 }
