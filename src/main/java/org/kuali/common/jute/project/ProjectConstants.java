2f93b51182a2d40674f158dcc3fb90804d17acee
Jeff Caddel <jcaddel@yahoo.com>
2015-01-21 16:06:59 -0700

    updates

--- src/main/java/org/kuali/common/jute/project/ProjectConstants.java
+++ src/main/java/org/kuali/common/jute/project/ProjectConstants.java
@@ -4,10 +4,10 @@ public final class ProjectConstants {
 
     private ProjectConstants() {}
 
-    // These two must always exactly match what is in the pom.xml
+    // These two must exactly match the entries in the pom
     private static final String GROUP_ID = "org.kuali.common";
     private static final String ARTIFACT_ID = "kuali-jute";
 
-    public static final ProjectIdentifier KUALI_JUTE_PROJECT_ID = DefaultProjectIdentifier.build(GROUP_ID, ARTIFACT_ID);
+    public static final ProjectIdentifier KUALI_JUTE_PROJECT_ID = ProjectIdentifier.build(GROUP_ID, ARTIFACT_ID);
 
 }
--- src/main/java/org/kuali/common/jute/project/ProjectIdentifier.java
+++ src/main/java/org/kuali/common/jute/project/ProjectIdentifier.java
@@ -1,9 +1,87 @@
 package org.kuali.common.jute.project;
 
-public interface ProjectIdentifier extends Comparable<ProjectIdentifier> {
+import static java.util.Objects.hash;
+import static org.kuali.common.jute.base.Objects.equalByComparison;
+import static org.kuali.common.jute.base.Precondition.checkNotBlank;
 
-    String getGroupId();
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.google.common.collect.ComparisonChain;
 
-    String getArtifactId();
+@JsonDeserialize(builder = ProjectIdentifier.Builder.class)
+public final class ProjectIdentifier implements Comparable<ProjectIdentifier> {
+
+    private final String groupId;
+    private final String artifactId;
+
+    private ProjectIdentifier(Builder builder) {
+        this.groupId = builder.groupId;
+        this.artifactId = builder.artifactId;
+    }
+
+    public static ProjectIdentifier build(String groupId, String artifactId) {
+        return builder().withGroupId(groupId).withArtifactId(artifactId).build();
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder implements org.apache.commons.lang3.builder.Builder<ProjectIdentifier> {
+
+        private String groupId;
+        private String artifactId;
+
+        public Builder withGroupId(String groupId) {
+            this.groupId = groupId;
+            return this;
+        }
+
+        public Builder withArtifactId(String artifactId) {
+            this.artifactId = artifactId;
+            return this;
+        }
+
+        @Override
+        public ProjectIdentifier build() {
+            return validate(new ProjectIdentifier(this));
+        }
+
+        private static ProjectIdentifier validate(ProjectIdentifier instance) {
+            checkNotBlank(instance.groupId, "groupId");
+            checkNotBlank(instance.artifactId, "artifactId");
+            return instance;
+        }
+    }
+
+    @Override
+    public int compareTo(ProjectIdentifier other) {
+        ComparisonChain chain = ComparisonChain.start();
+        chain = chain.compare(groupId, other.getGroupId());
+        chain = chain.compare(artifactId, other.getArtifactId());
+        return chain.result();
+    }
+
+    @Override
+    public int hashCode() {
+        return hash(groupId, artifactId);
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        return equalByComparison(this, object);
+    }
+
+    @Override
+    public String toString() {
+        return groupId + ":" + artifactId;
+    }
+
+    public String getGroupId() {
+        return groupId;
+    }
+
+    public String getArtifactId() {
+        return artifactId;
+    }
 
 }
