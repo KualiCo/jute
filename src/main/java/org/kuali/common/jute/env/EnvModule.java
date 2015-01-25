234fa3341bed3969a2b1ffeaf0003ece8730da36
Jeff Caddel <jcaddel@yahoo.com>
2015-01-25 08:03:25 -0700

    updates

--- src/main/java/org/kuali/common/jute/env/EnvModule.java
+++ src/main/java/org/kuali/common/jute/env/EnvModule.java
@@ -4,7 +4,10 @@ import static org.kuali.common.jute.env.EnvironmentVariables.alternateKeyFunctio
 
 import java.util.List;
 
+import org.kuali.common.jute.env.filter.annotation.CsvSplitter;
+
 import com.google.common.base.Function;
+import com.google.common.base.Splitter;
 import com.google.inject.AbstractModule;
 import com.google.inject.TypeLiteral;
 
@@ -12,6 +15,7 @@ public class EnvModule extends AbstractModule {
 
     @Override
     protected void configure() {
+        bind(Splitter.class).annotatedWith(CsvSplitter.class).toInstance(Splitter.on(',').omitEmptyStrings().trimResults());
         bind(new TypeLiteral<List<Function<String, String>>>() {}).annotatedWith(AlternateKeyFunctions.class).toInstance(alternateKeyFunctions());
         bind(Environment.class).to(StandardEnvironment.class).asEagerSingleton();
     }
