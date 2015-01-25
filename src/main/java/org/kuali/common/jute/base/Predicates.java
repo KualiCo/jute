fd590d6377be0391d3f87bec3576112a117eb211
Jeff Caddel <jcaddel@yahoo.com>
2015-01-25 04:58:50 -0700

    updates

--- src/main/java/org/kuali/common/jute/base/Predicates.java
+++ src/main/java/org/kuali/common/jute/base/Predicates.java
@@ -7,7 +7,10 @@ import static com.google.common.base.Predicates.containsPattern;
 import static com.google.common.base.Predicates.not;
 import static com.google.common.base.Predicates.or;
 import static com.google.common.collect.Iterables.isEmpty;
+import static com.google.common.collect.Lists.newArrayList;
 import static org.kuali.common.jute.base.Precondition.checkNotNull;
+
+import java.util.List;
 
 import com.google.common.base.Predicate;
 
@@ -15,23 +18,24 @@ public final class Predicates {
 
     private Predicates() {};
 
-    private static final Predicate<String> ALWAYS_TRUE = alwaysTrue();
-    private static final Predicate<String> ALWAYS_FALSE = alwaysFalse();
+    private static final Predicate<CharSequence> ALWAYS_TRUE = alwaysTrue();
+    private static final Predicate<CharSequence> ALWAYS_FALSE = alwaysFalse();
 
-    public static Predicate<String> includesExcludes(Iterable<String> includes, Iterable<String> excludes) {
+    public static Predicate<CharSequence> includesExcludes(Iterable<String> includes, Iterable<String> excludes) {
         checkNotNull(includes, "includes");
         checkNotNull(excludes, "excludes");
-        Predicate<String> incl = isEmpty(includes) ? ALWAYS_TRUE : containsAny(includes);
-        Predicate<String> excl = isEmpty(excludes) ? ALWAYS_FALSE : containsAny(excludes);
+        Predicate<CharSequence> incl = isEmpty(includes) ? ALWAYS_TRUE : containsAnyPattern(includes);
+        Predicate<CharSequence> excl = isEmpty(excludes) ? ALWAYS_FALSE : containsAnyPattern(excludes);
         return and(incl, not(excl));
     }
 
-    public static Predicate<String> containsAny(Iterable<String> patterns) {
+    public static Predicate<CharSequence> containsAnyPattern(Iterable<String> patterns) {
         checkNotNull(patterns, "patterns");
-        Predicate<String> predicate = alwaysFalse();
+        List<Predicate<CharSequence>> predicates = newArrayList();
         for (String pattern : patterns) {
-            predicate = or(containsPattern(pattern), predicate);
+            Predicate<CharSequence> predicate = containsPattern(pattern);
+            predicates.add(predicate);
         }
-        return predicate;
+        return or(predicates);
     }
 }
