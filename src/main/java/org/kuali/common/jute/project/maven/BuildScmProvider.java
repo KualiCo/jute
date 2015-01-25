942c7c0bf2ea59bd61f7bac09aaa8bb56ab8276e
Jeff Caddel <jcaddel@yahoo.com>
2015-01-25 08:27:32 -0700

    updates

--- src/main/java/org/kuali/common/jute/project/maven/BuildScmProvider.java
+++ src/main/java/org/kuali/common/jute/project/maven/BuildScmProvider.java
@@ -3,15 +3,18 @@ package org.kuali.common.jute.project.maven;
 import static com.google.common.base.Optional.absent;
 import static org.kuali.common.jute.base.Precondition.checkNotNull;
 
+import javax.inject.Inject;
 import javax.inject.Provider;
 
 import org.kuali.common.jute.project.BuildScm;
+import org.kuali.common.jute.project.maven.annotation.SkipProjectScm;
 
 import com.google.common.base.Optional;
 
 public final class BuildScmProvider implements Provider<Optional<BuildScm>> {
 
-    public BuildScmProvider(DirectoryContext dirs, Project project, boolean skip) {
+    @Inject
+    public BuildScmProvider(DirectoryContext dirs, Project project, @SkipProjectScm boolean skip) {
         this.dirs = checkNotNull(dirs, "dirs");
         this.project = checkNotNull(project, "project");
         this.skip = skip;
--- src/main/java/org/kuali/common/jute/project/maven/WriteMetadataModule.java
+++ src/main/java/org/kuali/common/jute/project/maven/WriteMetadataModule.java
@@ -1,5 +1,6 @@
 package org.kuali.common.jute.project.maven;
 
+import static java.lang.Boolean.parseBoolean;
 import static java.lang.System.currentTimeMillis;
 import static org.kuali.common.jute.base.Exceptions.illegalState;
 
@@ -7,10 +8,15 @@ import java.io.IOException;
 
 import org.kuali.common.jute.net.InetAddress;
 import org.kuali.common.jute.project.BuildEvent;
+import org.kuali.common.jute.project.BuildScm;
 import org.kuali.common.jute.project.annotation.BuildHost;
 import org.kuali.common.jute.project.annotation.BuildTimestamp;
+import org.kuali.common.jute.project.maven.annotation.SkipProjectScm;
 
+import com.google.common.base.Optional;
 import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import com.google.inject.TypeLiteral;
 
 public class WriteMetadataModule extends AbstractModule {
 
@@ -18,9 +24,16 @@ public class WriteMetadataModule extends AbstractModule {
     protected void configure() {
         bindConstant().annotatedWith(BuildTimestamp.class).to(currentTimeMillis());
         bind(InetAddress.class).annotatedWith(BuildHost.class).toInstance(buildLocalHost());
+        bind(new TypeLiteral<Optional<BuildScm>>() {}).toProvider(BuildScmProvider.class);
         bind(BuildEvent.class).toProvider(BuildEvent.Builder.class);
         bind(ProjectMetadata.class).toProvider(ProjectMetadata.Builder.class);
         bind(Runnable.class).toProvider(WriteMetadataRunnable.Builder.class);
+    }
+
+    @Provides
+    @SkipProjectScm
+    private boolean skipProjectScm(MavenEnvironment env) {
+        return parseBoolean(env.getProperty("project.scm.skip", "true"));
     }
 
     private static InetAddress buildLocalHost() {
