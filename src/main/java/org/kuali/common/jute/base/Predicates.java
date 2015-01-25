d4996b50ed90277874c1a27eab30664e32997e86
Jeff Caddel <jcaddel@yahoo.com>
2015-01-25 04:33:03 -0700

    updates

--- src/main/java/org/kuali/common/jute/base/Predicates.java
+++ src/main/java/org/kuali/common/jute/base/Predicates.java
@@ -32,6 +32,6 @@ public final class Predicates {
         for (String token : tokens) {
             predicate = or(containsPattern(token), predicate);
         }
-        return predicate;x
+        return predicate;
     }
 }
