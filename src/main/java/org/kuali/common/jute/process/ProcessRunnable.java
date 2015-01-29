a44eab408f0ffbb21ca55e827e1d7ff1a4d809c9
Jeff Caddel <jcaddel@yahoo.com>
2015-01-29 07:46:39 -0700

    updates

--- src/main/java/org/kuali/common/jute/process/ProcessRunnable.java
+++ src/main/java/org/kuali/common/jute/process/ProcessRunnable.java
@@ -1,26 +1,90 @@
 package org.kuali.common.jute.process;
 
+import static com.google.common.collect.Range.closed;
+import static java.lang.String.format;
 import static org.kuali.common.jute.base.Exceptions.illegalState;
-import static org.kuali.common.jute.base.Precondition.checkNotNull;
+import static org.kuali.common.jute.reflect.Reflection.checkNoNulls;
 
 import java.io.IOException;
 
-public final class ProcessRunnable implements Runnable {
+import javax.inject.Provider;
 
-    public ProcessRunnable(ProcessService service, ProcessContext context) {
-        this.service = checkNotNull(service, "service");
-        this.context = checkNotNull(context, "context");
-    }
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.google.common.base.Joiner;
+import com.google.common.base.VerifyException;
+import com.google.common.collect.Range;
+
+@JsonDeserialize(builder = ProcessRunnable.Builder.class)
+public final class ProcessRunnable implements Runnable {
 
     private final ProcessService service;
     private final ProcessContext context;
+    private final Range<Integer> allowedExitValues;
+    private final boolean validateExitValue;
 
     @Override
     public void run() {
         try {
-            service.execute(context);
+            ProcessResult result = service.execute(context);
+            int exitValue = result.getExitValue();
+            if (validateExitValue && !allowedExitValues.contains(exitValue)) {
+                String range = allowedExitValues.toString();
+                String command = context.getCommand();
+                String args = Joiner.on(' ').join(context.getArgs());
+                String msg = "invalid exit value '%s', allowed values %s.  [%s %s]";
+                throw new VerifyException(format(msg, exitValue, range, command, args));
+            }
         } catch (IOException e) {
             throw illegalState(e);
+        }
+    }
+
+    private ProcessRunnable(Builder builder) {
+        this.service = builder.service;
+        this.context = builder.context;
+        this.allowedExitValues = builder.allowedExitValues;
+        this.validateExitValue = builder.validateExitValue;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder implements org.apache.commons.lang3.builder.Builder<ProcessRunnable>, Provider<ProcessRunnable> {
+
+        private ProcessService service;
+        private ProcessContext context;
+        private Range<Integer> allowedExitValues = closed(0, 0);
+        private boolean validateExitValue = true;
+
+        public Builder withService(ProcessService service) {
+            this.service = service;
+            return this;
+        }
+
+        public Builder withContext(ProcessContext context) {
+            this.context = context;
+            return this;
+        }
+
+        public Builder withAllowedExitValues(Range<Integer> allowedExitValues) {
+            this.allowedExitValues = allowedExitValues;
+            return this;
+        }
+
+        public Builder withValidateExitValue(boolean validateExitValue) {
+            this.validateExitValue = validateExitValue;
+            return this;
+        }
+
+        @Override
+        public ProcessRunnable get() {
+            return build();
+        }
+
+        @Override
+        public ProcessRunnable build() {
+            return checkNoNulls(new ProcessRunnable(this));
         }
     }
 
@@ -30,6 +94,14 @@ public final class ProcessRunnable implements Runnable {
 
     public ProcessContext getContext() {
         return context;
+    }
+
+    public Range<Integer> getAllowedExitValues() {
+        return allowedExitValues;
+    }
+
+    public boolean isValidateExitValue() {
+        return validateExitValue;
     }
 
 }
--- src/test/java/org/kuali/common/jute/project/UnitTestInjection.java
+++ src/test/java/org/kuali/common/jute/project/UnitTestInjection.java
@@ -1,17 +1,30 @@
 package org.kuali.common.jute.project;
 
 import static com.google.common.collect.Iterables.concat;
+import static com.google.common.collect.Lists.newArrayList;
 import static com.google.inject.Guice.createInjector;
 import static java.util.Arrays.asList;
+
+import java.util.List;
 
 import org.kuali.common.jute.project.maven.KualiJuteModule;
 import org.kuali.common.jute.project.maven.ProjectModule;
 
+import com.google.common.collect.ImmutableList;
+import com.google.inject.AbstractModule;
 import com.google.inject.Injector;
 
 public final class UnitTestInjection {
 
     private UnitTestInjection() {}
+
+    public static List<AbstractModule> getUnitTestModules() {
+        List<AbstractModule> list = newArrayList();
+        list.addAll(newArrayList(new BasicModuleProvider().get()));
+        list.add(new KualiJuteModule());
+        list.add(new ProjectModule());
+        return ImmutableList.copyOf(list);
+    }
 
     public static Injector createUnitTestInjector() {
         return createInjector(concat(new BasicModuleProvider().get(), asList(new KualiJuteModule(), new ProjectModule())));
