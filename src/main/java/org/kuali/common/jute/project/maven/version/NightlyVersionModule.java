f82016ec7251494bf8206b9ea4c081c3119371df
Jeff Caddel <jcaddel@yahoo.com>
2015-01-29 07:05:24 -0700

    updates

--- src/main/java/org/kuali/common/jute/project/maven/version/NightlyVersionModule.java
+++ src/main/java/org/kuali/common/jute/project/maven/version/NightlyVersionModule.java
@@ -1,5 +1,7 @@
 package org.kuali.common.jute.project.maven.version;
 
+import org.kuali.common.jute.process.ProcessContext;
+import org.kuali.common.jute.process.ProcessRunnable;
 import org.kuali.common.jute.project.maven.annotation.Version;
 
 import com.google.inject.AbstractModule;
@@ -9,6 +11,8 @@ public class NightlyVersionModule extends AbstractModule {
     @Override
     protected void configure() {
         bind(String.class).annotatedWith(Version.class).toProvider(NightlyVersionProvider.class);
+        bind(ProcessContext.class).toProvider(SetVersionProcessContextProvider.class);
+        bind(Runnable.class).to(ProcessRunnable.class);
     }
 
 }
