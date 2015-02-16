7231c6fbd834072b3296596f477ef86b2dee2f92
Jeff Caddel <jcaddel@yahoo.com>
2015-02-16 16:22:15 -0700

    updates

--- src/test/java/org/kuali/common/jute/ant/AntTest.java
+++ src/test/java/org/kuali/common/jute/ant/AntTest.java
@@ -3,7 +3,6 @@ package org.kuali.common.jute.ant;
 import static com.google.common.collect.Iterables.concat;
 import static com.google.inject.Guice.createInjector;
 import static java.util.Arrays.asList;
-import static org.kuali.common.jute.base.Strings.flatten;
 import static org.kuali.common.jute.project.UnitTestInjection.getUnitTestModules;
 
 import java.util.List;
@@ -12,7 +11,6 @@ import org.junit.Test;
 import org.kuali.common.jute.ant.annotation.Targets;
 import org.kuali.common.jute.base.BaseUnitTest;
 
-import com.google.common.base.Function;
 import com.google.inject.Injector;
 import com.google.inject.Key;
 import com.google.inject.TypeLiteral;
@@ -31,21 +29,6 @@ public class AntTest extends BaseUnitTest {
         } catch (Throwable e) {
             e.printStackTrace();
         }
-    }
-
-    private static class TokenFunction implements Function<String, String> {
-
-        public TokenFunction(String prefix) {
-            this.prefix = prefix;
-        }
-
-        private final String prefix;
-
-        @Override
-        public String apply(String input) {
-            return flatten(prefix + input);
-        }
-
     }
 
 }
